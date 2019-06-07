cfaa69fd8a7d26b2440069e45e6eedbb27a271bb
MasafumiKabe <msfmkb@gmail.com>
2019-06-07 16:55:20 +0900

    video uploader

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
 
+  mount_uploader :video, VideoUploader
+
   validates :title, presence: true, length: { maximum: 255 }
-  # validates :video, presence: true
+  validates :video, presence: true
 end
